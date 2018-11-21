9e1d9ddf67d19451d922183c4a09e2880704b6cb
Andrew Korzhuev <korzhuev@andrusha.me>
2018-11-21 23:47:47 +0100

    Add 2.3.1, 2.3.2, and 2.4.0 Spark builds
    Also use SHA512 for checksums

--- build_image.sh
+++ build_image.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -xeo pipefail
 
-wget -q -O spark.tgz $SPARK_URL
-echo "$MD5_SUM spark.tgz" | md5sum -c -
+ARCHIVE_NAME=$(echo $SPARK_URL | grep -o -E '[^/]+$')
+
+wget -q $SPARK_URL -O $ARCHIVE_NAME
+echo "$SHA_CHECKLINE" | shasum -a 512 -c -s $SHA_CHECKIFLE
 mkdir -p spark
-tar -xvz --strip 1 -C spark -f spark.tgz
+tar -xvz --strip 1 -C spark -f $ARCHIVE_NAME
 pushd spark
 docker build -t andrusha/spark-k8s:$IMAGE_VERSION -f kubernetes/dockerfiles/spark/Dockerfile .
 popd
